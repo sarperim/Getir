fe2105bcde0f52d30cff308cfbe76d9eceef6f77
sarperim <ercansarperim@gmail.com>
2025-11-19 00:53:45 +0300

    Refactor: Convert Files to Class Libraries.

--- Data/Repository/Repositories/AddressRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Getir.Data.Models;
-using Getir.Data.Repository.Interfaces;
-
-namespace Getir.Data.Repository.Repositories
-{
-    public class AddressRepository(GetirDbContext db) : GenericRepository<Address>(db), IAddressRepository
-    {
-    }
-}
--- Data/Repository/Repositories/UserRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Getir.Data.Models;
-using Getir.Data.Repository.Interfaces;
-
-namespace Getir.Data.Repository.Repositories
-{
-    public class UserRepository(GetirDbContext db) : GenericRepository<User>(db), IUserRepository
-    {
-    }
-}
